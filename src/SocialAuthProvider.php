7b55b47291da64a9cd8e27833bfb01491671c4cc
vladyslavlopatin <ilopatin3@gmail.com>
2023-05-20 20:42:01 +0200

    LocaleWasChanged

--- src/SocialAuthProvider.php
+++ src/SocialAuthProvider.php
@@ -14,8 +14,11 @@ class SocialAuthProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-social-auth');
+
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'laravel-social-auth');
 
         $this->publishes([

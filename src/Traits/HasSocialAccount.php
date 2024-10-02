df77d45f72aa6a164a7b51e2c5330c7e0bfecf57
Vladyslav Lopatin <ilopatin3@gmail.com>
2024-10-02 14:29:28 +0200

    Update HasSocialAccount.php

--- src/Traits/HasSocialAccount.php
+++ src/Traits/HasSocialAccount.php
@@ -3,12 +3,9 @@
 namespace Atin\LaravelSocialAuth\Traits;
 
 use Atin\LaravelSocialAuth\Models\SocialAccount;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 trait HasSocialAccount
 {
-    use SoftDeletes;
-
     public function socialAccount()
     {
         return $this->hasOne(SocialAccount::class);
